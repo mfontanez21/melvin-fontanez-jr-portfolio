0c6a81e50c3d8548920e8f7a814fabb6084c683b
melvinf21 <melvinfontanez@gmail.com>
2023-05-30 18:42:04 -0400

    added more jobs to experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -25,6 +25,21 @@ const Experience = () => {
               <div className="text">
               In my role as Air Specialist at Lola.com, I was responsible for booking and managing flight reservations for passengers, utilizing both the Amadeus GDS system and the Lola.com web app. Additionally, I tackled complex corporate travel issues, employing innovative solutions to address recurring problems. I actively collaborated with customers and team members to troubleshoot and resolve issues, consistently striving to achieve a high level of customer satisfaction. Furthermore, I had the opportunity to closely collaborate with software engineers, contributing my insights and expertise to help develop new technologies.
               </div>
+              <h3>Crystal Travel and Tours</h3>
+              <h5>Travel Management Sales Associate,  03/2018 - 12/2019</h5>
+              <div className="text">
+              Taking on the position of a Travel Management Sales Associate, my responsibilities encompassed the booking and maintenance of flight, hotel, car, and train reservations using the Amadeus GDS System. Within this role, I efficiently handled ticket-related tasks such as issuing, exchanging, and refunding for both leisure and corporate travelers. Furthermore, I collaborated closely with airlines to facilitate smooth arrangements for large group travel, ensuring the seamless coordination of 15 or more travelers.
+              </div>
+              <h3>Road Scholar</h3>
+              <h5>Travel Services Advisor,  08/2016 - 02/2018</h5>
+              <div className="text">
+              In the capacity of a Travel Services Advisor, I undertook the essential duty of facilitating and managing flight, hotel, car, and train reservations using the Amadeus GDS system. Through collaborative consultations, I engaged with passengers to determine their unique needs and preferences, allowing me to arrange appropriate destinations, transportation modes, travel dates, costs, and accommodations. Furthermore, I provided valuable support to travelers by efficiently resolving emergency travel issues, addressing complaints, and facilitating timely refunds when required.
+              </div>
+              <h3>Grand Circle Travel</h3>
+              <h5>Air Travel Consultant,  02/2014 - 06/2016</h5>
+              <div className="text">
+              As an Air Travel Consultant, my main responsibilities revolved around booking and managing flight reservations for passengers through the Amadeus GDS system. Additionally, I actively engaged with passengers to understand their preferences and requirements, providing guidance on suitable destinations, transportation options, travel dates, costs, and accommodations. In the event of travel issues, complaints, or refund requests, I efficiently addressed and resolved them, ensuring customer satisfaction throughout their journey.
+              </div>
         </div>
       </div>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@
 import { LinkedIn } from "@mui/icons-material";
 import { Twitter } from "@mui/icons-material";
 import { GitHub } from "@mui/icons-material";
+import { Summarize } from "@mui/icons-material";
 import "../styles/Footer.css"
 import { Link } from "react-router-dom";
 
@@ -19,6 +20,9 @@ function footer () {
     <Link to ={"https://github.com/mfontanez21"} target="_blank" rel="noreferrer">
       <GitHub/>
     </Link>
+    <Link to ={"https://docs.google.com/document/d/1JXqHBYg8qAaVu-AYuT_rHteYo-1BV2QcOpSif41izzk/edit?usp=sharing"} target="_blank" rel = "noreferrer">
+      <Summarize/>
+      </Link>
     </div>
     <p> &copy; 2023 mfontanezjr.dev </p>
     </div>
